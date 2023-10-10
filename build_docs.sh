b861d3af7330fe43661d93f77408c4060840a65d
Yann Rouillard <yann@pleiades.fr.eu.org>
2023-10-10 22:42:20 +0200

    :construction_worker: Configure spoons CI/CD

---
+++ build_docs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eux
+
+if [ -d ../hammerspoon ]; then
+    HAMMERSPOON_PATH="../hammerspoon"
+elif [ -d ./hammerspoon ]; then
+    HAMMERSPOON_PATH="./hammerspoon"
+else
+    echo "Unable to find Hammerspoon"
+    exit 1
+fi
+
+mkdir -p .docs_tmp
+python3 "${HAMMERSPOON_PATH}/scripts/docs/bin/build_docs.py" -e "${HAMMERSPOON_PATH}/scripts/docs/templates/" -o .docs_tmp -i "Hammerspoon Spoons" -u "https://github.com/yannrouillard/Spoons/blob/master/" -j -t -n Source
+cp "${HAMMERSPOON_PATH}/scripts/docs/templates/docs.css" .docs_tmp/html/
+cp "${HAMMERSPOON_PATH}/scripts/docs/templates/jquery.js" .docs_tmp/html/
+mv .docs_tmp/html/* docs/
+mv .docs_tmp/docs{,_index}.json docs/
